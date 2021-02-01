b18fff8a86eba01cf59228715bd46113c18d88c7
alfonsojk <alfonso.juarezkelly@gmail.com>
2021-02-01 13:50:12 -0600

    heroku

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
     try{
         //mongodb connection string
-        const con = await mongoose.connect("http://localhost:27017/api/users",{
+        const con = await mongoose.connect("mongodb://localhost:27017/api/users",{
         useNewUriParser: true,
         useUnifiedTopology: true,
         useFindAndModify:false,
